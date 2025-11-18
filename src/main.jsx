35b9e05edfe72bdd9b0ac6292a3076e951197d31
SoleneSol <so.buono@gmail.com>
2025-11-18 14:06:47 +0100

    import react dom in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter } from "react-router-dom";
+import * as ReactDOM from "react-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <HashRouter>
