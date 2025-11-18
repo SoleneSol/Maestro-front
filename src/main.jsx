9cb72fbab97e4b873aa91737e630d1de91d7ff85
SoleneSol <so.buono@gmail.com>
2025-11-18 14:08:47 +0100

    back to strictmode in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter } from "react-router-dom";
-import * as ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <HashRouter>
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
         <App />
-    </HashRouter>
+    </StrictMode>
 );
