8a3357e7e8dd783f46a5f775c7034d84c7eab6aa
SoleneSol <so.buono@gmail.com>
2025-11-18 14:03:31 +0100

    reactDom added on main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <HashRouter>
         <App />
     </HashRouter>
